0651ff0893954bc5cfb02876504a9d89db5a7393
xXxYoLoSwAgxXx <harrisfambam@gmail.com>
2017-02-24 09:11:37 -1000

    HW text file complete

--- app.js
+++ app.js
@@ -5,17 +5,17 @@
 */
 function cars(){
 	var car1 = "Toyota";
-	var car2 = "Jeep Wrangler";
-	var car3 = "Mazda";
+	var car2 = "Smart Car";
+	var car3 = "Bat Mobile";
 
 	//Create an array declaration named cars and set each car variable as the values.
-
+	var cars = [car1, car2, car3];
 	//Print the created array
-
+	console.log(cars);
 	//Print the array length
-
+	console.log(cars.length);
 	//Return the created array and the array length
-	return;
+	return cars + cars.length;
 }
 
 console.log(cars());
@@ -28,13 +28,13 @@ console.log(cars());
 */
 function order(food, drink, dessert){
 	//Store the parameters in an array called "order"
-
+	var order =[food, drink, dessert];
 	//Print the array named "order"
-	
+	console.log(order);
 	//Print the length of the array named "order"
-
+	console.log(order.length);
 	//Return the order as a complete sentence represented as a string
-	return;
+	return"lets order: " + order[0] + " " + order[1] + " " + order[2] + " ";
 }
 
 console.log(order("Burger", "root beer", "frosty"));
@@ -57,9 +57,11 @@ var currentClasses = [
 */
 function loopToChange(classes){
 	//Print the parameter that is being passed through the function
-
+	console.log(classes);
 	//Use a For loop to iterate through each value in the passed array
-	
+	for (var i = classes.length - 1; i >=0; i--) {
+		console.log(classes[i]);
+	}
 	//Return classes
 	return;
 }
@@ -74,11 +76,26 @@ console.log(loopToChange(currentClasses));
 */
 function arrayMethodToLoop(numbers){
 	//Print the parameter that is being passed through the function
-
+	// console.log(numbers.length);
 	//Use the forEach() method to add 5 to each value in the passed array
+
+
+	numbers.forEach(function(curr){
+		// console.log('my current value', curr);
+		// console.log('my index', index);
+		// console.log('my array', arr);
+	  var current = curr + 5;
+		// console.log(curr + 5);
+		numbers.push(current);
+
+	var splices = numbers.splice(numbers.length, current);
+	// console.log(splices);
+		// var name = splices.push(curr + 5);
+		// console.log(name);
+	})
 	
 	//Return the changed array
-	return;
+	return ;
 }
 
 console.log(arrayMethodToLoop([5, 7, 3, 4, 5]));
@@ -91,17 +108,41 @@ console.log(arrayMethodToLoop([5 + 1, 3]));
 */
 function moreArrayMethods(priorities){
 	//Print the parameter that is being passed through the function
+	console.log(priorities);
 
 	//Use the method sort() to sort your priorities
+	priorities.sort();
+
+	console.log(priorities);
 
 	//Use the method indexOf() to replace "Not Going to Class" to "Going to Class"
 
+	var indexNumber = priorities.indexOf("Not Going to Class");
+
+	console.log(indexNumber);
+
+	priorities[indexNumber] = "Going to Class";
+	console.log(priorities);
+
+
+
 	//Use the method pop() to remove the item at the end of the array
 
+	priorities.pop();
+
+	console.log(priorities);
+
+
 	//Use the method push() to add a priority that is important to the success of this class and don't forget to sort() them afterwards!
+	priorities.push("take a break");
+	priorities.sort();
+
+	console.log(priorities);
+
+
 
 	//Return the changed array
-	return;
+	return priorities;
 }
 
 console.log(moreArrayMethods(["Zero Play", "Sleeping", "HW", "Work", "School", "Planning For Future", "Not Going to Class"]));
